e090a658d9034ed1a85c4884785cd36c6d91a611
Jhuan <juanbarrosdev@gmail.com>
2024-07-22 22:11:46 -0300

    Testando as funções do projeto.

---
+++ campo_minado/test/br/com/cod3r/cm/modelo/CampoTeste.java
@@ -0,0 +1,63 @@
+package br.com.cod3r.cm.modelo;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CampoTeste {
+	
+	private Campo campo = new Campo(3, 3);
+	
+	
+	
+	//Para cada novo teste, execute essa função
+	@BeforeEach
+	void iniciarCampo() {
+		campo = new Campo(3, 3);
+	}
+	
+	@Test
+	void testeVizinhoDistancia1Esquerda() {
+		Campo vizinho = new Campo(3, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado); // verifica se o teste foi bem sucedido ou não
+	}
+	
+	@Test
+	void testeVizinhoDistancia1Direita() {
+		Campo vizinho = new Campo(3, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado); // verifica se o teste foi bem sucedido ou não
+	}
+	
+	@Test
+	void testeVizinhoDistancia1EmCima() {
+		Campo vizinho = new Campo(2, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado); // verifica se o teste foi bem sucedido ou não
+	}
+	
+	@Test
+	void testeVizinhoDistancia1EmBaixo() {
+		Campo vizinho = new Campo(4, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado); // verifica se o teste foi bem sucedido ou não
+	}
+	
+	@Test
+	void testeVizinhoDistancia2() {
+		Campo vizinho = new Campo(2, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado); // verifica se o teste foi bem sucedido ou não
+	}
+	
+	@Test
+	void testeNaoVizinho() {
+		Campo vizinho = new Campo(1, 1);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertFalse(resultado);
+	}
+
+}
